9423f794f408875421696cef3fe68dfaa912e452
NJoaoPedro <160683169+NJoaoPedro@users.noreply.github.com>
2024-02-20 21:59:49 -0300

    Create ATIVIDADE1.java
    ATIVIDADE 1
    20/02

---
+++ ATIVIDADE1.java
@@ -0,0 +1,12 @@
+     
+        String nome = "Joao";
+        String sobrenome = "Pedro";
+        Integer ano = 2024;
+        int idade = 19;
+        int quantidade = 5;
+        double peso = 75,9;
+        char sexo = "M";
+        boolean confirmar = true;
+        float valor = 5,2;
+        Integer subtração = 10 - 4;
+        
