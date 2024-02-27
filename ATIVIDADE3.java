2f697ee674c26ac31cfe66afe94c63a9a576dbab
NJoaoPedro <160683169+NJoaoPedro@users.noreply.github.com>
2024-02-26 21:22:37 -0300

    Create ATIVIDADE3.java

---
+++ ATIVIDADE3.java
@@ -0,0 +1,7 @@
+String nomeDoAluno = "Joao"
+int idadeDoAluno = 19
+double notaDaProva1 = 8.5
+double notaDaProva2 = 5.8
+Integer mediaFinal = somaNotas / 2
+
+Integer somaNotas = notaDaProva1 + notaDaProva2
