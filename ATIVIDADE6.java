32d2f796f215a4658bc189c2fe004489dc226195
NJoaoPedro <160683169+NJoaoPedro@users.noreply.github.com>
2024-02-27 21:44:16 -0300

    Create ATIVIDADE6.java

---
+++ ATIVIDADE6.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+       
+        int numeroProduto = 123; 
+        double precoProduto = 2.5; 
+        
+        String descricaoProduto;
+        
+        descricaoProduto = "Fone de ouvido Bluetooth";
+        
+        System.out.println("Descricao do Produto: " + descricaoProduto);
+        
+        System.out.println("Dados do Produto:");
+        System.out.println("Numero: " + numeroProduto);
+        System.out.println("Preco: $" + precoProduto);
+        System.out.println("Descricao: " + descricaoProduto);
+    }
+}
