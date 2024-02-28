8a9209f9591b79876febfaa996e9a4f3d78cc7ec
NJoaoPedro <160683169+NJoaoPedro@users.noreply.github.com>
2024-02-27 21:58:26 -0300

    Create atividade5.java
    atividade 5
    não finalizada

---
+++ atividade5.java
@@ -0,0 +1,20 @@
+public class ControleEstoque {
+    public static void main(String[] args) {
+        
+        String nomeSmartphone = "Samsung Galaxy S23 Ultra";
+        String nomeLaptop = "Notebook Lenovo";
+        String nomeFone = "Fone de Ouvido On ear";
+        String marcaSmartphone = "Samsung";
+        String marcaLaptop = "Lenovo";
+        String marcaFone = "JBL";
+        double precoSmartphone = 5.699;
+        double precoLaptop = 2.583;
+        double precoFone = 223.19;
+        int quantSmartphone = 10;
+        int quantLaptop = 20;
+        int quantFone = 5;
+        
+        
+
+    }
+}
